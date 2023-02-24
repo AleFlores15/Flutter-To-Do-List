6583b095202530b53f48b7cfd7b07154df7fcfd8
unknown <alejandro.flores.a@ucb.edu.bo>
2023-02-23 23:25:37 -0400

    buttons created

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -14,13 +14,57 @@ class LoginScreen extends StatelessWidget {
         child: SafeArea(
           child: Form(
             key: _form,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Image.asset("images/Inicio.png"),
-                Text("Bienvenido a la Aplicacion", style: TextStyle(fontSize: 30),textAlign: TextAlign.center,)
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.asset("images/Inicio.png", height: 100, width: 100,),
+                  Text("Bienvenido a la Aplicacion", style: TextStyle(fontSize: 30),textAlign: TextAlign.center),
+                  SizedBox( height: 10,),
+                  Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(color: Colors.grey.shade200,borderRadius: BorderRadius.all(Radius.circular(60))),
+
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.face,color: Theme.of(context).primaryColor),
+                        labelStyle: TextStyle(color: Theme.of(context).primaryColor), 
+                        border: InputBorder.none,
+                        labelText: "Usuario"
+                      ),
+
+                    ),
+                  ),
+                  SizedBox(height:15),
+                  Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(color: Colors.grey.shade200,borderRadius: BorderRadius.all(Radius.circular(60))),
+
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock_outline_sharp,color: Theme.of(context).primaryColor),
+                        labelStyle: TextStyle(color: Theme.of(context).primaryColor), 
+                        border: InputBorder.none,
+                        labelText: "Usuario"
+                      ),
+
+                    ),
+                  ),
+                  SizedBox(height:15),
+                  ElevatedButton(onPressed: () {} ,child: Text("Login", style: TextStyle(fontSize: 20))),
+                  SizedBox(height:15),
+                  Row(
+                    
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                    
+                    ElevatedButton(onPressed: (){},child: Text("Registrarse"))
+                  ],)
+
+                ],
+              ),
             ),
           ),
         ),
