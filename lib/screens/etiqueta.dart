85e26cb00104b2ab239550590fe2eb68f784843b
unknown <alejandro.flores.a@ucb.edu.bo>
2023-02-24 18:59:15 -0400

    update created

--- lib/screens/etiqueta.dart
+++ lib/screens/etiqueta.dart
@@ -1,92 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_to_do_list/screens/tareas.dart';
-List<String> etiqueta  =['Algo','ALGO2','ALGO3'];
+
+List<String> etiqueta = ['Algo', 'ALGO2', 'ALGO3'];
+
 class FormularioEtiqueta extends StatefulWidget {
-  const FormularioEtiqueta({super.key});
+  const FormularioEtiqueta({Key? key}) : super(key: key);
   static final namePage = "formulario";
-  
 
   @override
   State<FormularioEtiqueta> createState() => _FormularioEtiquetaState();
 }
 
 class _FormularioEtiquetaState extends State<FormularioEtiqueta> {
-  final idForm=GlobalKey<FormState>();
-  Map<dynamic,dynamic> newLabel={};
+  final idForm = GlobalKey<FormState>();
+  Map<dynamic, dynamic> newLabel = {};
+
   @override
   Widget build(BuildContext context) {
-return Scaffold(
-  appBar: AppBar(
-    title: Text('Gestionar etiquetas'),
-  ),
-  body: (etiqueta.isNotEmpty)
-      ? ListView(
-          children: _crearTarea(),
-          physics: BouncingScrollPhysics(),
-        )
-      : Center(child: Text("No hay etiqueta")),
-  floatingActionButton: FloatingActionButton(
-    onPressed: () {
-      setState(() {
-        etiqueta.add("");
-        print(etiqueta.length);
-      });
-      Navigator.pushReplacement(context,
-          MaterialPageRoute(builder: (BuildContext context) => FormularioEtiqueta()));
-    },
-    child: Icon(Icons.add),
-  ),
-  bottomNavigationBar: Container(
-    padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-    child: ElevatedButton(
-      onPressed: () {
-        // TODO: handle button press
-      },
-      child: Text('Guardar'),
-    ),
-  ),
-);
-
-  }
-
-    List<Widget> _crearTarea() {
-  List<Widget> temp = [];
-
-  for (int i = 0; i < etiqueta.length; i++) {
-    Widget item = Row(
-      children: [
-        Expanded(
-          child: TextFormField(
-            onSaved: (valor) {
-              newLabel['Nombre'] = valor;
-            },
-            initialValue: etiqueta[i],
-            decoration: InputDecoration(hintText: "Nombre de la etiqueta"),
-          ),
-        ),
-        IconButton(
-          icon: Icon(Icons.delete),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gestionar etiquetas'),
+      ),
+      body: (etiqueta.isNotEmpty)
+          ? Form(
+              key: idForm,
+              child: ListView(
+                children: _crearTarea(),
+                physics: BouncingScrollPhysics(),
+              ),
+            )
+          : Center(child: Text("No hay etiqueta")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            etiqueta.add("");
+            print(etiqueta.length);
+          });
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => FormularioEtiqueta()));
+        },
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: ElevatedButton(
           onPressed: () {
+            idForm.currentState!.save();
             setState(() {
-              etiqueta.removeAt(i);
+              etiqueta = newLabel.values.toList().cast<String>();
             });
+            print("Funciona");
           },
+          child: Text('Guardar'),
         ),
-      ],
+      ),
     );
-    temp.add(item);
   }
 
-  return temp;
-}
+  List<Widget> _crearTarea() {
+    List<Widget> temp = [];
 
- void agrega() {
-    print(etiqueta.length);
-    etiqueta.add("a");
-    print(etiqueta.length);
-  
-}
+    for (int i = 0; i < etiqueta.length; i++) {
+      Widget item = Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              onSaved: (valor) {
+                newLabel[i] = valor;
+              },
+              initialValue: etiqueta[i],
+              decoration: InputDecoration(hintText: "Nombre de la etiqueta"),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              setState(() {
+                etiqueta.removeAt(i);
+              });
+            },
+          ),
+        ],
+      );
+      temp.add(item);
+    }
 
-
-
+    return temp;
+  }
 }
