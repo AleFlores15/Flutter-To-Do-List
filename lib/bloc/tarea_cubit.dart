db5135f302f190b05a6eb37e1ee8d8a398b7228e
unknown <alejandro.flores.a@ucb.edu.bo>
2023-04-21 00:56:52 -0400

    label state changed

--- lib/bloc/tarea_cubit.dart
+++ lib/bloc/tarea_cubit.dart
@@ -1,12 +1,36 @@
+
+import 'package:flutter_to_do_list/bloc/tarea_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'tarea_state.dart';
 
-
-class Tareass{
-  static List <String> labelsdata=[
-    'Trabajo',
-    'Casa',
-    'Personal',
-
+class Tarea{
+  static List <Tareas> tareasdata=[
+    
   ];
+}
+
+class TareasCubit extends Cubit<TareaState>{
+  TareasCubit() : super(TareaState(tareas: Tarea.tareasdata));
+
+  void addTarea (Tareas tarea){
+    Tarea.tareasdata.add(tarea);
+    emit(TareaState(tareas: Tarea.tareasdata));
+  }
+
+  void deleteTarea (Tareas tarea){
+    Tarea.tareasdata.remove(tarea);
+    emit(TareaState(tareas: Tarea.tareasdata));
+  }
+  void updateTarea (Tareas tarea, int index){
+    Tarea.tareasdata[index].completado = !Tarea.tareasdata[index].completado;
+    emit(TareaState(tareas: Tarea.tareasdata));
+  }
+ 
+
+
+  void saveTareas (){
+    Tarea.tareasdata = state.tareas;
+    emit(TareaState(tareas: Tarea.tareasdata));
+    
+  }
+  
 }
