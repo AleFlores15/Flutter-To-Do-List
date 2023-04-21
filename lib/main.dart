79ad12a43d27582dc2ad3a8ed6b67f7d054ecabd
unknown <alejandro.flores.a@ucb.edu.bo>
2023-04-21 07:19:31 -0400

    main problems solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cubit_todo/bloc/label_cubit.dart';
+import 'package:flutter_to_do_list/bloc/label_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import './screens/login.dart';
